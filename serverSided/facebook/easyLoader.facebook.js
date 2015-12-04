86175195581ebeb3f1f549a7527de27a107dd2c4
darthjee favini <darthjee@gmail.com>
2015-12-04 18:38:04 -0200

    Add end of line

--- serverSided/facebook/easyLoader.facebook.js
+++ serverSided/facebook/easyLoader.facebook.js
@@ -1,15 +1,15 @@
 (function(){
-  
+
     var scrpt = document.createElement('script');
     scrpt.setAttribute('type','text/javascript');
     scrpt.setAttribute('src', 'http://dl.dropbox.com/u/5993666/gms/easyLoader/easyLoader.js');
     document.querySelector('head').appendChild(scrpt);
-    
+
   var watcher = setInterval(function(){
     if (typeof easyLoader == "function")
     {
       var loader = easyLoader();
-      
+
       loader.add({
         url:'http://dl.dropbox.com/u/5993666/gms/facebook/jq.js'
       });
@@ -44,10 +44,8 @@
         }
       });
       loader.run();
-      
-      
-      
+
       clearInterval(watcher);
     }
   }, 500);
-})()
+})();
