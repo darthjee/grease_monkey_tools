4352187261e6a8cccf03f69c701dc1ba93f95485
darthjee favini <darthjee@gmail.com>
2015-12-04 18:26:29 -0200

    Add moon

---
+++ serverSided/moon/easy-loader.moon.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name            Moon Script Loader
+// @description     Script de carregamento de scripts para moon
+// @include         http://cristinaefernando.net/
+// @author      Fernando DarthJee Favini
+// @version         0.0.1
+// ==/UserScript==
+
+(function(){
+  var url = 'http://scripts.favinis.net/serverSided/moon/easy-loader.moon.js';
+    var scrpt = document.createElement('script');
+    scrpt.setAttribute('type','text/javascript');
+    scrpt.setAttribute('src', url);
+    document.querySelector('head').appendChild(scrpt);
+})();
