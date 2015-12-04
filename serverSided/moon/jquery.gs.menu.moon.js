269c95f4513cf0d954759226624e23f29bd55f13
darthjee favini <darthjee@gmail.com>
2015-12-04 18:51:11 -0200

    Add mooon menu config

---
+++ serverSided/moon/jquery.gs.menu.moon.js
@@ -0,0 +1,20 @@
+(function($,Menu){
+  Menu.moon = {};
+
+  var precolandia = function(){
+    var html = '';
+    var set = {
+      html : html,
+      btnTitle:'precolandia',
+      extraClass:'',
+      render:function(){
+        return window.location.href.match(/.*/);
+      },
+      bind:function(){
+        var controler = this;
+      }
+    };
+    return Menu.wing(set);
+  };
+  Menu.moon.precolandia = precolandia;
+})($jq,$jq.fn.menu);
