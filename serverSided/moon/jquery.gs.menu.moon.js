f67f26aacf01dbafeb506d501ea58aafcb6531c3
darthjee favini <darthjee@gmail.com>
2015-12-05 17:20:19 -0200

    Log gift information

--- serverSided/moon/jquery.gs.menu.moon.js
+++ serverSided/moon/jquery.gs.menu.moon.js
@@ -2,8 +2,8 @@
   Menu.moon = {};
 
   var precolandia = function(){
-    var html = '<input type="text" class="store_id" />' +
-    '<input type="text" class="marriage_id" />' +
+    var html = '<input type="text" class="store_id" /> <br />' +
+    '<input type="text" class="marriage_id" /> <br />' +
     '<button>import</button>';
     var set = {
       html : html,
@@ -13,24 +13,57 @@
         return window.location.href.match(/www.\precolandia\.com\.br/);
       },
       bind:function(){
-        var controler = this,
-            $div = controler.div,
-            $button = $div.find('button'),
-            $store_id = $div.find('.store_id'),
-            $marriage_id = $div.find('.marriage_id');
-
-        $button.click(function() {
-          console.info({
-            marriage_id: $marriage_id.val(),
-            store_id: $store_id.val()
-          });
-        });
-
-        $store_id.cookiefy('GMS-prcl-store-id');
-        $marriage_id.cookiefy('GMS-prcl-marriage-id');
+        var controler = new Controller(this);
+        controler.start();
       }
     };
     return Menu.wing(set);
+  };
+
+  function Controller(controller) {
+    this.div = controler.div;
+  }
+
+  var fn = Controller.prototype;
+
+  fn.submit = function() {
+    var $store_id = this.div.find('.store_id'),
+        $marriage_id = this.div.find('.marriage_id');
+
+     console.info({
+        marriage_id: $marriage_id.val(),
+        store_id: $store_id.val(),
+        gifts: this.getGifts()
+      });
+  };
+
+  fn.start = function() {
+    var $button = this.div.find('button'),
+        that = this;
+
+    $button.click(function() {
+      that.submit();
+    });
+
+    $store_id.cookiefy('GMS-prcl-store-id');
+    $marriage_id.cookiefy('GMS-prcl-marriage-id');
+  };
+
+  fn.getGifts = function () {
+    var $blocks = $('.border_listagem_right_cinza_claro');
+
+    return $blocks.map(this.parseGift);
+  };
+
+  fn.parseGift = function() {
+    var $block = $(this),
+        $link = $block.find('a');
+
+    return {
+      image_url: $block.find('img').attr('src'),
+      url: $link.attr('href'),
+      name: $link.text
+    };
   };
 
   Menu.moon.precolandia = precolandia;
