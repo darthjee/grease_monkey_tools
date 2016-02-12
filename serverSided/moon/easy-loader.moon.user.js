ca2bae84ac6ee277b74aa8e9a0a098b5ab2c2574
darthjee <darthjee@gmail.com>
2016-02-12 10:59:45 -0200

    Add precolandia url

--- serverSided/moon/easy-loader.moon.user.js
+++ serverSided/moon/easy-loader.moon.user.js
@@ -2,13 +2,14 @@
 // @name            Moon Script Loader
 // @description     Script de carregamento de scripts para moon
 // @include         http://www.pontofrio.com.br/Site/ListaGerenciadaLandingPage.aspx?*
+// @include         http://www.precolandia.com.br/lista+de+casamento*
 // @author      Fernando DarthJee Favini
-// @version         0.0.1
+// @version         0.0.3
 // ==/UserScript==
 
 (function(){
-  var url = 'http://scripts.favinis.net/serverSided/moon/easy-loader.moon.js';
-    var scrpt = document.createElement('script');
+    var url = 'http://scripts.favinis.net/serverSided/moon/easy-loader.moon.js',
+        scrpt = document.createElement('script');
     scrpt.setAttribute('type','text/javascript');
     scrpt.setAttribute('src', url);
     document.querySelector('head').appendChild(scrpt);
