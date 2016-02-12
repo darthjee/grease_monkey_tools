5c8e20fba81363a48101b3104c3d6bb23cb24a2d
darthjee <darthjee@gmail.com>
2016-02-12 18:48:15 -0200

    Add fastshop

--- serverSided/moon/easy-loader.moon.user.js
+++ serverSided/moon/easy-loader.moon.user.js
@@ -4,8 +4,10 @@
 // @include         http://www.pontofrio.com.br/Site/ListaGerenciadaLandingPage.aspx?*
 // @include         http://www.precolandia.com.br/lista+de+casamento*
 // @include         http://www.precolandia.com.br/giftlistview.aspx?*
+// @include         http://listadecasamento.fastshop.com.br/ListaCasamento/ListaCasamentoPresentesComprados.aspx
+// @include         http://listadecasamento.fastshop.com.br/ListaCasamento/ListaCasamentoAguardandoComprador.aspx
 // @author      Fernando DarthJee Favini
-// @version         0.0.4
+// @version         0.0.6
 // ==/UserScript==
 
 (function(){
